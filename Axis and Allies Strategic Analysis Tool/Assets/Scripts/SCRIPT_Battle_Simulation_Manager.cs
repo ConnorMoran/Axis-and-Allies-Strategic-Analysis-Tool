793edad049c5c182095a7bcfa79787d3800557df
ConnorMoran <connor.moran.17@cnu.edu>
2025-07-31 14:04:19 -0400

    init commit

---
+++ Axis and Allies Strategic Analysis Tool/Assets/Scripts/SCRIPT_Battle_Simulation_Manager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SCRIPT_Battle_Simulation_Manager : MonoBehaviour
+{
+    // This script is responsible for managing the battle simulation in the game.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
